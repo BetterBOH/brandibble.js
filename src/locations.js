20ae4cccecfaf6e8badade8ed526eacf78213a3d
Sebastian <sebastian@sanctuary.computer>
2019-02-06 15:58:30 -0500

    adds optional queryObject to be passed ot locations show

--- src/locations.js
+++ src/locations.js
@@ -16,22 +16,28 @@ export default class Locations {
   }
 
   show(locationId, lat, lng, serviceType, requestedAt, includeTimes) {
-    let formattedRequestedAt;
-
-    if (requestedAt) {
-      formattedRequestedAt = coerceDateToISO8601(requestedAt);
+    /**
+     * If the second arg passed in (lat) is an object
+     * we can assume a queryObject was passed (preferred)
+     * over a set of query params as separate arguments.
+     * This allows us to be backwards compatible, by supporting
+     * the latter while supporting the preferred query object
+     */
+    let queryParamObject;
+    if (lat && typeof lat === 'object') {
+      queryParamObject = lat;
+    } else {
+      queryParamObject = Object.assign(
+        {},
+        !!lat && { latitude: lat },
+        !!lng && { longitude: lng },
+        !!serviceType && { service_type: serviceType },
+        !!requestedAt && { requested_at: coerceDateToISO8601(requestedAt) },
+        !!includeTimes && { include_times: includeTimes },
+      );
     }
 
     const queryStringBase = `locations/${locationId}`;
-    const queryParamObject = Object.assign(
-      {},
-      !!lat && { latitude: lat },
-      !!lng && { longitude: lng },
-      !!serviceType && { service_type: serviceType },
-      !!formattedRequestedAt && { requested_at: formattedRequestedAt },
-      !!includeTimes && { include_times: includeTimes },
-    );
-
     if (Object.keys(queryParamObject).length) {
       return this.adapter.request(
         'GET',
