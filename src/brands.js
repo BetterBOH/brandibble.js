ab8184f094abf8bf709d4ccfd9769bcbe3e9f105
Joshie Fishbein <joshie@sanctuary.computer>
2019-02-15 14:11:16 -0500

    add Brands class

---
+++ src/brands.js
@@ -0,0 +1,13 @@
+export default class Brands {
+  constructor(adapter) {
+    this.adapater = adapter;
+  }
+
+  current() {
+    return this.show()
+  }
+
+  show() {
+    return this.adapter.request('GET', null, null);
+  }
+};
