a11aadb6b75b76174fabb391c88baa7ab3bf52b7
Joshie Fishbein <joshie@sanctuary.computer>
2019-02-15 14:55:14 -0500

    adjust brand stub to match our test brand, change brand test to expect response to match new stub

--- spec/stubs/brand.stub.js
+++ spec/stubs/brand.stub.js
@@ -1,27 +1,28 @@
 export default {
-  app_image: "//s3.amazonaws.com/betterboh/u/img/local/2/1550203474_9f22dad043acbb6414c6.jpg",
-  apple_touch_icon: "//s3.amazonaws.com/betterboh/u/img/prod/2/1537984306_open-tender-favicon_300x300.png",
-  default_sender: "contact@brandibble.co",
-  description: "Brandibble is a new online ordering platform built from the ground up to be mobile-friendly and fully customizable.",
+  app_image: "",
+  apple_touch_icon: "//s3.amazonaws.com/betterboh/u/img/prod/6/1461763438_Rabbit_Icon.jpg",
+  default_sender: "contact@diginn.com",
+  description: "We serve seasonal American food, mostly vegetables.",
   display_allergens: 1,
   display_nutritional_info: 1,
   display_tags: 1,
-  domain: "demo.brandibble.co",
-  favicon: "//s3.amazonaws.com/betterboh/u/img/prod/2/1537984306_open-tender-favicon.ico",
+  domain: "diginn.com",
+  favicon: "//s3.amazonaws.com/betterboh/u/img/staging/6/1546306519_dig-inn-favicon.ico",
   ga_tracking_id: "",
-  gtm_container_id: "GTM-583QQ5K",
-  gtm_custom: 0,
+  gtm_container_id: "GTM-NGNXXVC",
+  gtm_custom: 1,
   intercom: 1,
-  large_image: "//s3.amazonaws.com/betterboh/u/img/local/2/1550203475_7a3c6f9134fd992514f3.jpg",
-  logo: "//s3.amazonaws.com/betterboh/u/img/prod/2/1547658324_open-tender-black-gradient_600x81.png",
-  map_marker: "",
-  name: "Brandibble",
+  large_image: "",
+  logo: null,
+  map_marker: "//s3.amazonaws.com/betterboh/u/img/prod/6/1519329777_DigFlag_Black.png",
+  name: "Dig Inn",
   order_types: {
     catering: [
       "pickup",
-      "delivery"
+      "delivery",
+      "walkin"
     ],
-    merch: [
+    merch:[
       "pickup",
       "delivery",
       "walkin"
@@ -32,8 +33,8 @@ export default {
       "walkin"
     ]
   },
-  short_name: "Brandibble Demo",
-  small_image: "//s3.amazonaws.com/betterboh/u/img/local/2/1550203478_f01646fe1487c59f0f19.jpg",
-  title: "Brandibble",
-  website_url: "https://demo.brandibble.co/"
-}
+  short_name: "Dig Inn",
+  small_image: "",
+  title: "Dig Inn",
+  website_url: ""
+};
--- src/brands.js
+++ src/brands.js
@@ -1,13 +1,18 @@
 export default class Brands {
   constructor(adapter) {
-    this.adapater = adapter;
+    this.adapter = adapter;
   }
 
   current() {
     return this.show()
   }
 
+  /**
+   * Currently the brands#show endpoint is the same as adapter.apiBase. So we 
+   * can pass an empty string as the path in order to form a request at the root
+   * of the apiBase.
+   */
   show() {
-    return this.adapter.request('GET');
+    return this.adapter.request('GET', '');
   }
 };
