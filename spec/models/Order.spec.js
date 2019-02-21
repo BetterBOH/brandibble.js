e7d2fefd539ca0a63a4d574ede8051da50d4e8f5
Ellis Marte <elliscmarte@gmail.com>
2019-02-21 15:26:10 -0500

    add reset tip

--- spec/models/Order.spec.js
+++ spec/models/Order.spec.js
@@ -226,6 +226,16 @@ describe('models/order', () => {
     });
   });
 
+  it('can reset tip', () => {
+    const newOrder = new Brandibble.Order(Brandibble.adapter, locationJSON.location_id, 'delivery');
+    return newOrder.setTip('credit', 2.50).then(() => {
+      expect(newOrder.miscOptions.tip).to.deep.equal(2.50);
+      return newOrder.resetTip().then(() => {
+        expect(newOrder.miscOptions.tip).to.equal(null);
+      });
+    });
+  });
+
   it('can set payment method to levelup', () => {
     const newOrder = new Brandibble.Order(Brandibble.adapter, locationJSON.location_id, 'pickup');
     return newOrder.setPaymentMethod(PaymentTypes.LEVELUP).then(() => {
