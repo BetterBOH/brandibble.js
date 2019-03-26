6f5fceacd2a4439dfb03683b556c2e1f2769db02
Sebastian Odell <sebastian@sanctuary.computer>
2019-03-26 09:14:00 -0400

    passed include_times by default to locations#show and locations#index endpoints (#179)

--- spec/adapter.spec.js
+++ spec/adapter.spec.js
@@ -16,7 +16,8 @@ describe('Adapter', () => {
     Brandibble.adapter.apiBase = 'http://httpstat.us/';
     Brandibble.adapter.apiKey = null;
     return Brandibble.adapter.request('GET', '200/cors').catch((error) => {
-      Brandibble.adapter.apiBase = 'https://staging.brandibble.co/api/v1/brands/6/';
+      Brandibble.adapter.apiBase =
+        'https://staging.brandibble.co/api/v1/brands/6/';
       Brandibble.adapter.apiKey = UnsecureApiKey;
       expect(error.response).to.present;
       expect(error.exception).to.present;
@@ -27,53 +28,68 @@ describe('Adapter', () => {
   });
 
   it('can persist currentOrder to localStorage', () => {
-    return configureTestingOrder(Brandibble).then((order) => {
-      return Brandibble.adapter.persistCurrentOrder(order).then((savedOrder) => {
-        expect(order).to.equal(savedOrder);
-        expect(savedOrder).to.be.an.instanceof(Brandibble.Order);
-      });
-    }).catch(error => console.log(error));
+    return configureTestingOrder(Brandibble)
+      .then((order) => {
+        return Brandibble.adapter
+          .persistCurrentOrder(order)
+          .then((savedOrder) => {
+            expect(order).to.equal(savedOrder);
+            expect(savedOrder).to.be.an.instanceof(Brandibble.Order);
+          });
+      })
+      .catch(error => console.log(error));
   });
 
-
   it('can restore currentOrder from localStorage', () => {
-    return configureTestingOrder(Brandibble).then((order) => {
-      // This is here for testing that this is properly restored... you'd never set it like this in a real app.
-      order.wantsFutureOrder = true;
-      return Brandibble.adapter.persistCurrentOrder(order).then(() => {
-        return Brandibble.adapter.restoreCurrentOrder().then((retrievedOrder) => {
-          expect(order).to.deep.equal(retrievedOrder);
-          expect(retrievedOrder).to.be.an.instanceof(Brandibble.Order);
+    return configureTestingOrder(Brandibble)
+      .then((order) => {
+        // This is here for testing that this is properly restored... you'd never set it like this in a real app.
+        order.wantsFutureOrder = true;
+        return Brandibble.adapter.persistCurrentOrder(order).then(() => {
+          return Brandibble.adapter
+            .restoreCurrentOrder()
+            .then((retrievedOrder) => {
+              expect(order).to.deep.equal(retrievedOrder);
+              expect(retrievedOrder).to.be.an.instanceof(Brandibble.Order);
+            });
         });
-      });
-    }).catch(error => console.log(error));
+      })
+      .catch(error => console.log(error));
   });
 
   it('can restore requestedAt from localStorage', () => {
-    return configureTestingOrder(Brandibble).then((order) => {
-      const requestedAtTime = `${(new Date()).toISOString().split('.')[0]}Z`;
-      return order.setRequestedAt(requestedAtTime).then(() => {
-        return Brandibble.adapter.restoreCurrentOrder().then((retrievedOrder) => {
-          expect(retrievedOrder.requestedAt).to.equal(requestedAtTime);
+    return configureTestingOrder(Brandibble)
+      .then((order) => {
+        const requestedAtTime = `${new Date().toISOString().split('.')[0]}Z`;
+        return order.setRequestedAt(requestedAtTime).then(() => {
+          return Brandibble.adapter
+            .restoreCurrentOrder()
+            .then((retrievedOrder) => {
+              expect(retrievedOrder.requestedAt).to.equal(requestedAtTime);
+            });
         });
-      });
-    }).catch(error => console.log(error));
+      })
+      .catch(error => console.log(error));
   });
 
   it('can will flush lifecycle specific state when flushAll is called', () => {
-    return configureTestingOrder(Brandibble).then((order) => {
-      expect(Brandibble.adapter.currentOrder).to.equal(order);
-      return Brandibble.adapter.flushAll().then(() => {
-        expect(Brandibble.adapter.currentOrder).to.be.null;
-      });
-    }).catch(error => console.log(error));
+    return configureTestingOrder(Brandibble)
+      .then((order) => {
+        expect(Brandibble.adapter.currentOrder).to.equal(order);
+        return Brandibble.adapter.flushAll().then(() => {
+          expect(Brandibble.adapter.currentOrder).to.be.null;
+        });
+      })
+      .catch(error => console.log(error));
   });
 
   it('can cut off slow requests based on the timeout property', () => {
     Brandibble.adapter.requestTimeout = 1;
     return Brandibble.locations.index().catch((err) => {
       Brandibble.adapter.requestTimeout = 10000;
-      expect(err.message).to.equal('Brandibble.js: The GET request to locations timed out after 1.');
+      expect(err.message).to.equal(
+        'Brandibble.js: The GET request to locations?include_times=true timed out after 1.',
+      );
       return true;
     });
   });
