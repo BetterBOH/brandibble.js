d602f9a27c9ac43cf973f934b7e91348e9cd2e3b
Sebastian <sebastian@sanctuary.computer>
2019-03-27 12:25:56 -0400

    fixes failing customer login test

--- spec/customers.spec.js
+++ spec/customers.spec.js
@@ -1,5 +1,11 @@
 /* global Brandibble expect it describe beforeEach window */
-import { seedEmail, seedText, shouldSucceed, shouldError, OrdersTestingUser } from './helpers';
+import {
+  seedEmail,
+  seedText,
+  shouldSucceed,
+  shouldError,
+  OrdersTestingUser,
+} from './helpers';
 
 describe('Customers', () => {
   beforeEach(() => window.localStorage.clear());
@@ -7,36 +13,48 @@ describe('Customers', () => {
   it('exists', () => expect(Brandibble.customers).to.exist);
 
   it('can create a customer', () => {
-    return Brandibble.customers.create({
-      first_name: 'Sanctuary',
-      last_name: 'Testing',
-      email: seedEmail(),
-      password: 'password',
-    }).then((response) => {
-      const data = shouldSucceed(response);
-      expect(data).to.have.property('customer_id');
-      expect(data).to.have.property('email');
-      expect(data).to.have.property('first_name');
-      expect(data).to.have.property('last_name');
-      expect(data).to.have.property('phone');
-    });
+    return Brandibble.customers
+      .create({
+        first_name: 'Sanctuary',
+        last_name: 'Testing',
+        email: seedEmail(),
+        password: 'password',
+      })
+      .then((response) => {
+        const data = shouldSucceed(response);
+        expect(data).to.have.property('customer_id');
+        expect(data).to.have.property('email');
+        expect(data).to.have.property('first_name');
+        expect(data).to.have.property('last_name');
+        expect(data).to.have.property('phone');
+      });
   });
 
   it('create fails with bad inputs', () => {
-    return Brandibble.customers.create({
-      first_name: 'Sanctuary',
-      last_name: 'Testing',
-      email: 'nope',
-      password: 'password',
-    }).catch((response) => {
-      const errors = shouldError(response);
-      expect(errors).to.be.a('array').to.have.lengthOf(2);
-      expect(errors[0]).to.have.property('code', 'customers.create.validation');
-      expect(errors[1]).to.have.property('title', 'Please submit a valid email address.');
-    });
+    return Brandibble.customers
+      .create({
+        first_name: 'Sanctuary',
+        last_name: 'Testing',
+        email: 'nope',
+        password: 'password',
+      })
+      .catch((response) => {
+        const errors = shouldError(response);
+        expect(errors)
+          .to.be.a('array')
+          .to.have.lengthOf(2);
+        expect(errors[0]).to.have.property(
+          'code',
+          'customers.create.validation',
+        );
+        expect(errors[1]).to.have.property(
+          'title',
+          'Please submit a valid email address.',
+        );
+      });
   });
 
-  it('can create and authenticate a customer', async function() {
+  it('can create and authenticate a customer', async () => {
     const seededEmail = seedEmail();
     const response = await Brandibble.customers.createAndAuthenticate({
       first_name: 'Sanctuary',
@@ -55,7 +73,7 @@ describe('Customers', () => {
     expect(Brandibble.adapter.customerToken).to.not.exist;
   });
 
-  it('create and authenticate fails with bad inputs', async function() {
+  it('create and authenticate fails with bad inputs', async () => {
     try {
       await Brandibble.customers.createAndAuthenticate({
         first_name: 'Sanctuary',
@@ -66,41 +84,59 @@ describe('Customers', () => {
     } catch (error) {
       const errors = shouldError(error);
 
-      expect(errors).to.be.a('array').to.have.lengthOf(2);
+      expect(errors)
+        .to.be.a('array')
+        .to.have.lengthOf(2);
       expect(errors[0]).to.have.property('code', 'customers.create.validation');
-      expect(errors[1]).to.have.property('title', 'Please submit a valid email address.');
+      expect(errors[1]).to.have.property(
+        'title',
+        'Please submit a valid email address.',
+      );
     }
   });
 
   it('can retrieve a token', () => {
-    return Brandibble.customers.token({
-      email: 'sanctuary-testing-customer@example.com',
-      password: 'password',
-    }).then((response) => {
-      const data = shouldSucceed(response);
-      expect(data).to.have.property('token');
-    });
+    return Brandibble.customers
+      .token({
+        email: 'sanctuary-testing-customer@example.com',
+        password: 'password',
+      })
+      .then((response) => {
+        const data = shouldSucceed(response);
+        expect(data).to.have.property('token');
+      });
   });
 
   it('fails to retrieve a token with bad inputs', () => {
-    return Brandibble.customers.token({
-      email: 'sanctuary-testing-customer@example.com',
-      password: 'nope',
-    }).catch((response) => {
-      const errors = shouldError(response);
-      expect(errors).to.be.a('array').to.have.lengthOf(2);
-      expect(errors[0]).to.have.property('code', 'customers.login.validation.brandibble');
-    });
+    return Brandibble.customers
+      .token({
+        email: 'sanctuary-testing-customer@example.com',
+        password: 'nope',
+      })
+      .catch((response) => {
+        const errors = shouldError(response);
+        expect(errors)
+          .to.be.a('array')
+          .to.have.lengthOf(2);
+
+        expect(errors[0]).to.have.property(
+          'code',
+          'customers.login.validation.both',
+        );
+      });
   });
 
-  it('can authenticate the client with a current customer', async function() {
+  it('can authenticate the client with a current customer', async () => {
     const response = await Brandibble.customers.authenticate({
       email: 'sanctuary-testing-customer@example.com',
       password: 'password',
     });
     const data = await shouldSucceed(response);
 
-    expect(data).to.have.property('email', 'sanctuary-testing-customer@example.com');
+    expect(data).to.have.property(
+      'email',
+      'sanctuary-testing-customer@example.com',
+    );
     // Customer Token is set in local storage
     expect(Brandibble.adapter.customerToken).to.be.a('string');
 
@@ -108,8 +144,10 @@ describe('Customers', () => {
     expect(Brandibble.adapter.customerToken).to.not.exist;
   });
 
-  it('can validate a customers metadata', async function() {
-    const response = await Brandibble.customers.validateCustomer({ email: 'sanctuary-testing-customer@example.com' });
+  it('can validate a customers metadata', async () => {
+    const response = await Brandibble.customers.validateCustomer({
+      email: 'sanctuary-testing-customer@example.com',
+    });
     const data = await shouldSucceed(response);
     expect(data).to.have.property('is_brandibble_active');
     expect(data).to.have.property('is_brandibble_customer');
@@ -119,19 +157,26 @@ describe('Customers', () => {
   });
 
   it('cannot validate a customers metadata when email is invalid', () => {
-    return Brandibble.customers.validateCustomer({ email: 'sanctuary-testing-customer@example.com' }).catch((response) => {
-      const errors = shouldError(response);
-      expect(errors).to.be.a('array');
-      expect(errors[0]).to.have.property('code', 'customers.validate.invalid_email');
-    });
+    return Brandibble.customers
+      .validateCustomer({ email: 'sanctuary-testing-customer@example.com' })
+      .catch((response) => {
+        const errors = shouldError(response);
+        expect(errors).to.be.a('array');
+        expect(errors[0]).to.have.property(
+          'code',
+          'customers.validate.invalid_email',
+        );
+      });
   });
 
-  it('can trigger a customers reset password flow', async function() {
-    const response = await Brandibble.customers.resetPassword({ email: 'sanctuary-testing-customer@example.com' });
+  it('can trigger a customers reset password flow', async () => {
+    const response = await Brandibble.customers.resetPassword({
+      email: 'sanctuary-testing-customer@example.com',
+    });
     expect(response).to.be.true;
   });
 
-  it('can attempt to complete a customer\'s reset password flow', async function() {
+  it("can attempt to complete a customer's reset password flow", async () => {
     try {
       await Brandibble.customers.finishResetPassword('123141254jlasdfjwqer', {
         password: 'newpassword',
@@ -139,79 +184,108 @@ describe('Customers', () => {
     } catch (response) {
       const errors = shouldError(response);
       expect(errors).to.be.a('array');
-      expect(errors[0]).to.have.property('code', 'customers.reset.invalid_token');
+      expect(errors[0]).to.have.property(
+        'code',
+        'customers.reset.invalid_token',
+      );
     }
   });
 
   it('can not show a customer that does not belong to the current token', () => {
-    return Brandibble.customers.authenticate({
-      email: 'sanctuary-testing-customer@example.com',
-      password: 'password',
-    }).then(() => {
-      return Brandibble.customers.show(1).catch((response) => {
-        const errors = shouldError(response);
-        expect(errors).to.have.lengthOf(1);
-        expect(errors[0]).to.have.property('code', 'customers.show.token_mismatch');
+    return Brandibble.customers
+      .authenticate({
+        email: 'sanctuary-testing-customer@example.com',
+        password: 'password',
+      })
+      .then(() => {
+        return Brandibble.customers.show(1).catch((response) => {
+          const errors = shouldError(response);
+          expect(errors).to.have.lengthOf(1);
+          expect(errors[0]).to.have.property(
+            'code',
+            'customers.show.token_mismatch',
+          );
+        });
       });
-    });
   });
 
   it('can update the current customer', () => {
-    return Brandibble.customers.authenticate({
-      email: 'sanctuary-testing-customer@example.com',
-      password: 'password',
-    }).then(() => {
-      const newLastName = seedText();
-      return Brandibble.customers.updateCurrent({
-        last_name: newLastName,
-      }).then((response) => {
-        const data = shouldSucceed(response);
-        expect(data.last_name).to.be.a('string', newLastName);
+    return Brandibble.customers
+      .authenticate({
+        email: 'sanctuary-testing-customer@example.com',
+        password: 'password',
+      })
+      .then(() => {
+        const newLastName = seedText();
+        return Brandibble.customers
+          .updateCurrent({
+            last_name: newLastName,
+          })
+          .then((response) => {
+            const data = shouldSucceed(response);
+            expect(data.last_name).to.be.a('string', newLastName);
+          });
       });
-    });
   });
 
   it('can not update a customer that does not own the current token', () => {
-    return Brandibble.customers.authenticate({
-      email: 'sanctuary-testing-customer@example.com',
-      password: 'password',
-    }).then(() => {
-      const newLastName = seedText();
-      return Brandibble.customers.update({ last_name: newLastName }, 5).catch((response) => {
-        const errors = shouldError(response);
-        expect(errors).to.have.lengthOf(1);
-        expect(errors[0]).to.have.property('code', 'customers.update.token_mismatch');
+    return Brandibble.customers
+      .authenticate({
+        email: 'sanctuary-testing-customer@example.com',
+        password: 'password',
+      })
+      .then(() => {
+        const newLastName = seedText();
+        return Brandibble.customers
+          .update({ last_name: newLastName }, 5)
+          .catch((response) => {
+            const errors = shouldError(response);
+            expect(errors).to.have.lengthOf(1);
+            expect(errors[0]).to.have.property(
+              'code',
+              'customers.update.token_mismatch',
+            );
+          });
       });
-    });
   });
 
   it('can check for qr code from levelup', () => {
-    return Brandibble.customers.authenticate({
-      email: 'sanctuary-testing-customer@example.com',
-      password: 'password',
-    }).then(() => {
-      return Brandibble.customers.currentLevelUpQRCode().catch((response) => {
-        const errors = shouldError(response);
-        expect(errors).to.have.lengthOf(1);
-        expect(errors[0]).to.have.property('code', 'customers.levelup.qr_code.show.missing_token');
+    return Brandibble.customers
+      .authenticate({
+        email: 'sanctuary-testing-customer@example.com',
+        password: 'password',
+      })
+      .then(() => {
+        return Brandibble.customers.currentLevelUpQRCode().catch((response) => {
+          const errors = shouldError(response);
+          expect(errors).to.have.lengthOf(1);
+          expect(errors[0]).to.have.property(
+            'code',
+            'customers.levelup.qr_code.show.missing_token',
+          );
+        });
       });
-    });
   });
 
   it('can check for loyalty from levelup', () => {
-    return Brandibble.customers.authenticate({
-      email: 'sanctuary-testing-customer@example.com',
-      password: 'password',
-    }).then(() => {
-      return Brandibble.customers.currentLevelUpLoyalty().catch((response) => {
-        const errors = shouldError(response);
-        expect(errors).to.have.lengthOf(1);
-        expect(errors[0]).to.have.property('code', 'customers.levelup.loyalty.show.missing_token');
+    return Brandibble.customers
+      .authenticate({
+        email: 'sanctuary-testing-customer@example.com',
+        password: 'password',
+      })
+      .then(() => {
+        return Brandibble.customers.currentLevelUpLoyalty().catch((response) => {
+          const errors = shouldError(response);
+          expect(errors).to.have.lengthOf(1);
+          expect(errors[0]).to.have.property(
+            'code',
+            'customers.levelup.loyalty.show.missing_token',
+          );
+        });
       });
-    });
   });
 
-  it('can check for customer loyalties', async function() {
+  it('can check for customer loyalties', async () => {
     const response = await Brandibble.customers.authenticate({
       email: 'sanctuary-orders-testing-customer@example.com',
       password: 'password',
@@ -222,7 +296,7 @@ describe('Customers', () => {
     expect(loyalties).to.be.array;
   });
 
-  it('can retrieve a users orders', async function() {
+  it('can retrieve a users orders', async function () {
     this.timeout(10000);
     const response = await Brandibble.customers.authenticate({
       email: 'sanctuary-orders-testing-customer@example.com',
@@ -234,13 +308,17 @@ describe('Customers', () => {
     expect(orders).to.be.array;
   });
 
-
-  it('can retrieve a users upcoming orders', async function() {
+  it('can retrieve a users upcoming orders', async function () {
     this.timeout(10000);
     const { email, password } = OrdersTestingUser;
-    const response = await Brandibble.customers.authenticate({ email, password });
+    const response = await Brandibble.customers.authenticate({
+      email,
+      password,
+    });
     const { customer_id } = response.data;
-    const data = await Brandibble.customers.orders(customer_id, { status: 'upcoming' });
+    const data = await Brandibble.customers.orders(customer_id, {
+      status: 'upcoming',
+    });
     const orders = await shouldSucceed(data);
     expect(orders).to.be.array;
   });
@@ -249,16 +327,23 @@ describe('Customers', () => {
    * for this to work, and given our test case here isn't setup, we're just testing
    * the failure case for now.
    */
-  it('can retrieve levelup campaign info for a user', async function() {
-    return Brandibble.customers.authenticate({
-      email: 'sanctuary-testing-customer@example.com',
-      password: 'password',
-    }).then(() => {
-      return Brandibble.customers.currentLevelUpCampaign(1, 'basic_v1').catch((response) => {
-        const errors = shouldError(response);
-        expect(errors).to.have.lengthOf(1);
-        expect(errors[0]).to.have.property('code', 'customers.levelup.campaigns.show.missing_token');
+  it('can retrieve levelup campaign info for a user', async () => {
+    return Brandibble.customers
+      .authenticate({
+        email: 'sanctuary-testing-customer@example.com',
+        password: 'password',
+      })
+      .then(() => {
+        return Brandibble.customers
+          .currentLevelUpCampaign(1, 'basic_v1')
+          .catch((response) => {
+            const errors = shouldError(response);
+            expect(errors).to.have.lengthOf(1);
+            expect(errors[0]).to.have.property(
+              'code',
+              'customers.levelup.campaigns.show.missing_token',
+            );
+          });
       });
-    });
   });
 });
